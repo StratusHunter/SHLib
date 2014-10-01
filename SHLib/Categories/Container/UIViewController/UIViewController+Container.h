8d9979abbdb748c1665d8e49af4b11aadd4da253
Terence Baker <terence@bulbstudios.com>
2014-10-01 10:22:52 +0100

    Changed empty functions in SHTableViewDelegate to protocols. Added comments for clarity

--- SHLib/Categories/Container/UIViewController/UIViewController+Container.h
+++ SHLib/Categories/Container/UIViewController/UIViewController+Container.h
@@ -6,6 +6,7 @@
 
 @interface UIViewController (Container)
 
+/** Swap sub controllers making sure that both controllers views conform to the size of the parent **/
 - (void)containerTransitionFromViewController:(UIViewController *)fromViewController toViewController:(UIViewController *)toViewController duration:(NSTimeInterval)duration options:(UIViewAnimationOptions)options animations:(void (^)(void))animations completion:(void (^)(BOOL finished))completion NS_AVAILABLE_IOS(5_0);
 
 @end
